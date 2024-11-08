44dd60b513f2bbd89edbb40ecefd5834ea71a89d
Caike Bittin <ikee.bittin@gmail.com>
2024-11-08 19:52:07 -0300

    feat: add authentication clerk

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,29 +1,38 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
   return (
     <div className="grid h-full grid-cols-2">
       {/* ESQUERDA */}
       <div className="mx-auto flex flex-col h-full max-w-[550px] justify-center p-8">
         <Image
           src="/logo.png"
-          width={173}
-          height={39}
+          width={185}
+          height={50}
           alt="Fluxy Logo"
-          className="mb-8"
+          className="mb-5"
         />
-        <h1 className="text-4xl font-bold mb-3">Bem vindo</h1>
+        <h1 className="text-4xl font-bold mb-3">Bem-vindo</h1>
         <p className="text-muted-foreground mb-8">
           O Fluxy é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant={"outline"}>
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant={"outline"}>
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
       {/* DIREITA */}
       <div className="relative h-full w-full">
