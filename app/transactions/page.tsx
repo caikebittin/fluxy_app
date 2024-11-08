cd8945039c5fe07271edb28299de7d3430257119
Caike Bittin <ikee.bittin@gmail.com>
2024-11-08 13:00:25 -0300

    feat: add login page

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,5 @@
-const Transaction = () => {
-    return (<h1>transaction Page</h1>);
-}
+const TransactionPage = () => {
+  return <h1>transaction Page</h1>;
+};
 
-export default Transaction;
+export default TransactionPage;
